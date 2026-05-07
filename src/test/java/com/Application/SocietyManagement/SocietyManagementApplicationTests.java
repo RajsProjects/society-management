45ba5ee1e6617e3906f9aea0723d5477aa60ec11
Aache_wale_bhaiya <sumanmandal8955@gmail.com>
2026-05-07 12:54:41 +0530

    fix: remove SpringBootTest annotation from context load test

--- src/test/java/com/Application/SocietyManagement/SocietyManagementApplicationTests.java
+++ src/test/java/com/Application/SocietyManagement/SocietyManagementApplicationTests.java
@@ -1,20 +1,10 @@
 package com.Application.SocietyManagement;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
-@TestPropertySource(properties = {
-		"spring.data.mongodb.uri=mongodb://localhost:27017/test",
-		"jwt.secret=TestSecretKeyForCIThatIsAtLeast32BytesLong=",
-		"jwt.expiration=86400000",
-		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration,org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration"
-})
 class SocietyManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
